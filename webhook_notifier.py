bcee09a2c5bdfc64c8116ba2d505c1eb9f34d821
purpl3rain <98284286+purpl3rain@users.noreply.github.com>
2022-01-31 12:38:51 -0800

    Add files via upload

---
+++ webhook_notifier.py
@@ -0,0 +1,13 @@
+from discord import Webhook, RequestsWebhookAdapter
+import json
+
+
+def notify_discord_channel(channel, username, content):
+    disc_channel = json.load(open('Discord/webhooks.json'))
+    try:
+        channel_id = disc_channel[channel]['channel_id']
+        token_id = disc_channel[channel]['token_id']
+        webhook = Webhook.partial(channel_id, token_id, adapter=RequestsWebhookAdapter())
+        webhook.send(content, username=username)
+    except KeyError:
+        print('No Channel named %s found - Notification was not sent' % channel)
